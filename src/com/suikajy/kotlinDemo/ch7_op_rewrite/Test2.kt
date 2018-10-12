554dd2235a2db8c8a7d552b098f49784462a3a4a
suikajy <2058550226@qq.com>
2018-10-12 13:58:52 +0800

    ch7 operator rewrite

--- src/com/suikajy/kotlinDemo/ch7_op_rewrite/Test2.kt
+++ src/com/suikajy/kotlinDemo/ch7_op_rewrite/Test2.kt
@@ -1,6 +1,7 @@
 package com.suikajy.kotlinDemo.ch7_op_rewrite
 
 import java.lang.IndexOutOfBoundsException
+import java.time.LocalDate
 
 operator fun Point.get(index: Int): Int {
     return when (index) {
@@ -22,6 +23,32 @@ operator fun MutablePoint.set(index: Int, value: Int) {
     }
 }
 
+data class Rectangle(val upperLeft: Point, val lowerRight: Point)
+
+operator fun Rectangle.contains(p: Point): Boolean {
+    // 构建一个区间，检查坐标x是否属于这个区间
+    return p.x in upperLeft.x until lowerRight.x &&
+            p.y in upperLeft.y until lowerRight.y
+}
+
+//operator fun <T : Comparable<T>> T.rangeTo(that: T): ClosedRange<T> {
+//
+//}
+
+
+
+operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> =
+        object : Iterator<LocalDate> {
+            var current = start
+
+            override fun next(): LocalDate = current.apply {
+                current = plusDays(1)
+            }
+
+            override fun hasNext(): Boolean =
+                    current <= endInclusive
+        }
+
 fun main(args: Array<String>) {
     val p = Point(10, 20)
     println(p[1]) // 20
@@ -29,4 +56,22 @@ fun main(args: Array<String>) {
     val mp = MutablePoint(10, 20)
     mp[1] = 42
     println(mp) // MutablePoint(x=10, y=42)
+
+    val rect = Rectangle(Point(10, 20), Point(50, 50))
+    println(Point(20, 30) in rect) // true
+    println(Point(5, 5) in rect) // false
+
+    val now = LocalDate.now()
+    val vacation = now..now.plusDays(10)
+    println(now.plusWeeks(1) in vacation) // true
+
+    val newYear = LocalDate.ofYearDay(2017, 1)
+    val daysOff = newYear.minusDays(1)..newYear
+
+    for (dayOff in daysOff) {
+        println(dayOff)
+    }
+    //2016-12-31
+    //2017-01-01
 }
+
---
+++ src/com/suikajy/kotlinDemo/ch7_op_rewrite/Test3.kt
@@ -0,0 +1,36 @@
+package com.suikajy.kotlinDemo.ch7_op_rewrite
+
+class Point2(val x: Int, val y: Int) {
+    operator fun component1() = x
+    operator fun component2() = y
+}
+
+data class NameComponents(val name: String, val extension: String)
+
+fun splitFilename(fullName: String): NameComponents {
+    val result = fullName.split('.', limit = 2)
+    return NameComponents(result[0], result[1])
+}
+
+fun splitFilename2(fullName: String): NameComponents {
+    val (name, extension) = fullName.split('.', limit = 2)
+    return NameComponents(name, extension)
+}
+
+fun printEntries(map: Map<String, String>) {
+    // 在
+    for ((key, value) in map) {
+        println("$key -> $value")
+    }
+}
+
+fun main(args: Array<String>) {
+    val p = Point(10, 20)
+    val (x, y) = p
+    println(x) // 10
+    println(y) // 20
+
+    val (name, ext) = splitFilename("example.kt")
+    println(name) // example
+    println(ext) // kt
+}
